f57a7da1c3808e00fbafcdd1b31e6ec748e0df6b
LaurensH2000 <laurenshoogenboom2000@gmail.com>
2025-04-18 17:30:43 +0200

    test

--- src/routes/login/+layout.svelte
+++ src/routes/login/+layout.svelte
@@ -12,7 +12,7 @@
         align-items: center;
         height: 100vh;
         width: 100vw;
-        background-color: rgb(250,250,250);
+        background: var(--grey-background-gradient);
 
         .authentication-form-wrapper {
             width: 100%;
