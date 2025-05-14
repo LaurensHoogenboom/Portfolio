475d7537e3563aea3b3d9ca9abf6c0d88f3bbc38
LaurensH2000 <laurenshoogenboom2000@gmail.com>
2025-05-14 15:39:10 +0200

    notice styling

--- src/lib/components/atoms/notice.svelte
+++ src/lib/components/atoms/notice.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { CircleCheck, Info, TriangleAlert } from '@lucide/svelte';
     import { fly, slide } from 'svelte/transition';
 
     let { message, type } : { 
@@ -9,16 +10,25 @@
 
 <style>
     .notice {
-            --color: blue;
-        padding: 10px;
+        --color: blue;
+        display: grid;
+        grid-template-columns: max-content minmax(0, 1fr);
+        grid-gap: var(--padding-4);
         border: 1px solid var(--color);
         border-radius: 5px;
         background-color: rgba(from var(--color) r g b / 0.2);
         margin-bottom: 15px;
+        padding: var(--padding-4) var(--padding-3);
+        align-items: center;
+
+        .icon-wrapper :global(svg) {
+            color: var(--color);
+            height: 20px;
+        }
 
         p {
             color: var(--color);
-            padding-bottom: 0;
+            padding-bottom: 2px;
         }
 
         &.warning {
@@ -36,5 +46,15 @@
 </style>
 
 <div class="notice {type}" in:fly={{ y: 20 }} out:slide>
+    <div class="icon-wrapper"> 
+        {#if type == "warning"}
+            <TriangleAlert />
+        {:else if type == "success"}
+            <CircleCheck />
+        {:else if type == "neutral"}
+            <Info />
+        {/if}
+    </div>
+
     <p>{message}</p>
 </div>
