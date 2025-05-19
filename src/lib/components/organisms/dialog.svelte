911e497695201d5aaaf5b7bd9524f6bf387ca019
LaurensH2000 <laurenshoogenboom2000@gmail.com>
2025-05-19 19:19:00 +0200

    scroll

--- src/lib/components/organisms/dialog.svelte
+++ src/lib/components/organisms/dialog.svelte
@@ -7,67 +7,6 @@
     let { children, title, closeCallback }: { children: Snippet, title?: string, closeCallback?: () => void } = $props();
 </script>
 
-<style>
-    .dialog {
-        position: fixed;
-        top: 50%;
-        left: 50%;
-        transform: translateY(-50%) translateX(-50%);
-        width: 400px;
-        height: 600px;
-        background: none;
-        border: none;
-        z-index: 100;
-
-        > .box {
-            position: relative;
-            flex-direction: column;
-            --border-size: var(--padding-4);
-            
-
-            &:after {
-                display: block;
-                content: '';
-                z-index: -1;
-                position: absolute;
-                top: calc(0px - var(--border-size));
-                bottom: calc(0px - var(--border-size));
-                left: calc(0px - var(--border-size));
-                right: calc(0px - var(--border-size));
-                background-color: rgba(from var(--white-background) r g b / 0.28);
-                border-radius: var(--border-radius-l);
-            }
-        }
-
-        .dialog-title {
-            display: grid;
-            grid-template-columns: 1fr max-content;
-            align-items: center;
-            min-height: 50px;
-
-            p {
-                padding-bottom: 0;
-            }
-        }
-
-        :global(.form-actions) {
-            grid-gap: var(--padding-3)
-        }
-    }
-
-    .backdrop {
-        display: flex;
-        position: fixed;
-        top: 0;
-        bottom: 0;
-        right: 0;
-        left: 0;
-        width: 100%;
-        height: 100vh;
-        z-index: 99;
-    }
-</style>
-
 <div class="dialog" in:slide={{ duration: 300 }} out:scale={{ duration: 300 }}>
     <div class="box">
         {#if title}
@@ -80,7 +19,9 @@
             </div>
         {/if}
 
-        {@render children()}
+        <div class="dialog-content">
+            {@render children()}
+        </div>
     </div>    
 </div>
 
