156e1329d26c41e7a8f11af4f5692777e4e3bef1
LaurensH2000 <49717394+LaurensHoogenboom@users.noreply.github.com>
2026-01-22 16:15:02 +0100

    Small SEO fixes
    * meta description
    
    * favicons
    
    * buttonsitemnames

--- src/routes/(site)/+layout.svelte
+++ src/routes/(site)/+layout.svelte
@@ -1,29 +1,27 @@
 <script lang="ts">
-    import type { Snippet } from 'svelte';
-    import type { LayoutData } from './$types';
-    import { dev } from '$app/environment';
+	import type { Snippet } from 'svelte';
+	import type { LayoutData } from './$types';
+	import { dev } from '$app/environment';
 	import Nav from '$siteComponents/sections/nav.svelte';
-    import Footer from '$siteComponents/sections/footer.svelte';
+	import Footer from '$siteComponents/sections/footer.svelte';
 
-    let { data, children }: { data: LayoutData, children: Snippet } = $props();
+	let { data, children }: { data: LayoutData; children: Snippet } = $props();
 </script>
 
 <svelte:head>
-    {#if dev}
-        <link rel="stylesheet" type="text/css" media="screen" href="/src/styles/site/site.css">
-    {:else}
-        <link rel="stylesheet" type="text/css" media="screen" href="/styles/site/site.css">
-    {/if}
-
-    <title>Laurens Hoogenboom</title>
+	{#if dev}
+		<link rel="stylesheet" type="text/css" media="screen" href="/src/styles/site/site.css" />
+	{:else}
+		<link rel="stylesheet" type="text/css" media="screen" href="/styles/site/site.css" />
+	{/if}
 </svelte:head>
 
 <div class="site-wrapper">
-    <Nav/>
+	<Nav />
 
-    <main>
-        {@render children()}
-    </main>
+	<main>
+		{@render children()}
+	</main>
 
-    <Footer />
+	<Footer />
 </div>
