e50a90d42b4317f0eab3139bf7cf5e41254d4490
LaurensH2000 <49717394+LaurensHoogenboom@users.noreply.github.com>
2026-01-24 15:10:07 +0100

    Gif Support

--- src/lib/utils/uploads/image/uploadImage.ts
+++ src/lib/utils/uploads/image/uploadImage.ts
@@ -9,11 +9,23 @@ const uploadImage = async (image: File | Buffer, title: string): Promise<Upload>
     const imageBuffer = image instanceof File ? await image.arrayBuffer() : image;
     const metaData = await sharp(imageBuffer).metadata();
 
-    const fullImage = await sharp(imageBuffer).webp().resize(1920, undefined).toBuffer();
-    const thumbnail = await sharp(imageBuffer).webp().resize(500, undefined).toBuffer();
+    let fullImage: Buffer<ArrayBufferLike>;
+    let thumbnail: Buffer<ArrayBufferLike>;
+    let targetFormat: 'webp' | 'gif' = 'webp';
 
-    const fullImageName = `${title.replace(/[^a-zA-Z0-9]/g, "")}.webp`;
-    const thumbnailImageName = `${title.replace(/[^a-zA-Z0-9]/g, "")}-thumnail.webp`;
+    if (metaData.format == 'gif') {
+        fullImage = (metaData.width > 1920)
+            ? await sharp(imageBuffer, { animated: true }).resize(1920, undefined).toBuffer()
+            : await sharp(imageBuffer, { animated: true }).toBuffer();
+        thumbnail = await sharp(imageBuffer, { animated: false }).resize(500, undefined).toBuffer();
+        targetFormat = 'gif';
+    } else {
+        fullImage = await sharp(imageBuffer).webp().resize(1920, undefined).toBuffer();
+        thumbnail = await sharp(imageBuffer).webp().resize(500, undefined).toBuffer();
+    }
+
+    const fullImageName = `${title.replace(/[^a-zA-Z0-9]/g, "")}.${targetFormat}`;
+    const thumbnailImageName = `${title.replace(/[^a-zA-Z0-9]/g, "")}-thumnail.${targetFormat}`;
 
     const fullImageUrl = getWriteUrl(fullImageName, 'image');
     const thumbnailImageUrl = getWriteUrl(thumbnailImageName, 'image');
