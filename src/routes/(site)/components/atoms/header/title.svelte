bb75d92a6088104809a5ecc35f7f082fae52abed
LaurensH2000 <laurenshoogenboom2000@gmail.com>
2026-02-16 19:57:01 +0100

    Revert "header layout fix"
    This reverts commit 2ff6f6649b12ec3d5b902a87c44e6ceb481f9d3a.

--- src/routes/(site)/components/atoms/header/title.svelte
+++ src/routes/(site)/components/atoms/header/title.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
-	import { typewriter } from '$lib/utils/transitions/typewriter';
+    import { typewriter } from "$lib/utils/transitions/typewriter";
 
-	let i = $state(0);
+    let i = $state(0);
 
-	const messages = ['Onderzoek', 'Vormgeving', 'Tekeningen'];
+	const messages = [
+		"Onderzoek",
+		"Vormgeving",
+		"Tekeningen"
+	]
 
 	$effect(() => {
 		const interval = setInterval(() => {
@@ -18,30 +22,23 @@
 </script>
 
 <h1>
-	{#key i}
-		<span in:typewriter={{ speed: 0.9 }}>
-			{messages[i] || ''}
-		</span>
-	{/key}
+    {#key i}
+        <span in:typewriter={{ speed: 0.9 }}>
+            {messages[i] || ''}
+        </span>
+    {/key}
 
-	&nbsp;
+    &nbsp;
 </h1>
 
 <style>
-	h1 {
-		@media (min-width: 1500px) {
-			font-size: 90px;
-		}
-	}
-
-	h1:after {
-		content: 'D';
-		opacity: 0.3;
-		font-size: 280px;
+    h1:after {
+        content: 'D';
+        opacity: 0.3;
 
 		@media (max-width: 1500px) {
 			content: 'E';
 			left: 30%;
 		}
-	}
+    }
 </style>
